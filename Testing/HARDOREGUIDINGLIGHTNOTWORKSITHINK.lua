b8bab61710d75db3bfd615772b2112ad3568cb46
Anctarus7105 <idzhabkin@gmail.com>
2026-02-03 14:34:35 +0400

    Create HARDOREGUIDINGLIGHTNOTWORKSITHINK.lua

---
+++ Testing/HARDOREGUIDINGLIGHTNOTWORKSITHINK.lua
@@ -0,0 +1,3 @@
+game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.Health.Music.Blue.SoundId = "rbxassetid://10472612727";
+
+game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game.Health.Music.Blue.Pitch = 0.55
