f9c2debd682a6a625eb78038e807dbf7e640629c
Anctarus7105 <idzhabkin@gmail.com>
2026-04-26 16:34:51 +0400

    Create ChatMessage.lua

---
+++ Hard Mode/ChatMessage.lua
@@ -0,0 +1,3 @@
+local prefix = " "
+local msg = "Harder Mode Loaded! by Anctarus#7105"
+game.TextChatService.TextChannels.RBXGeneral:DisplaySystemMessage(`<font color='#fff70f'>{prefix..msg}</font>`)
