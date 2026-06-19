7ea99690f30591ac59a397ab68eff7cb746d00ae
Anctarus7105 <idzhabkin@gmail.com>
2026-06-19 14:54:29 +0400

    Add script to display success message in chat

---
+++ jfgedwrgeg.lua
@@ -0,0 +1,3 @@
+local prefix = " "
+local msg = "[Oof's Impossible Mode]: Script succesfully executed."
+game.TextChatService.TextChannels.RBXGeneral:DisplaySystemMessage(`<font color='#00CF00'>{prefix..msg}</font>`)
